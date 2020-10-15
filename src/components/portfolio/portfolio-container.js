6ab7395b604d14917f034e952a038238c7d70c42
bolicoding <bolicoding@gmail.com>
2020-10-15 16:25:07 -0600

    Custom Functions in React and Using Map to Loop Over Data

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -2,19 +2,27 @@ import React, { Component } from "react";
 import PortfolioItem from "./portfolio-item";
 
 
-
 export default class PortfolioContainer extends Component {
   constructor() {
     super();
 
     console.log('it rendered')
   }
+
+  portfolioItems() {
+    const data = ["Fast Garage", "FFL", "OTC", "FedEx"];
+
+    return data.map(item => {
+      return <PortfolioItem />;
+    })
+  }
+
   render() {
     return (
       <div>
         <h2>Portfolio items go here...</h2>
 
-        <PortfolioItem /> 
+        {this.portfolioItems()}
       </div>
     );
   }

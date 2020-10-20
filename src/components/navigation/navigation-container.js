8d8470b319e24cde0f30d329cdef7ce579bc8f0b
bolicoding <bolicoding@gmail.com>
2020-10-20 16:17:42 -0600

    navlink active styles

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -9,16 +9,22 @@ export default class NavigationComponent extends Component {
   render() {
     return (
       <div>
-        <NavLink exact to="/">
+        <NavLink exact to="/" activeClassName="nav-link-active">
           Home
         </NavLink>
 
-        <NavLink to="/about-me">
+        <NavLink to="/about-me" activeClassName="nav-link-active">
           About
         </NavLink>
 
-        <button>Contact</button>
-        <button>Blog</button>
+        <NavLink to="/contact" activeClassName="nav-link-active">
+          Contact
+        </NavLink>
+
+        <NavLink to="/blog" activeClassName="nav-link-active">
+          Blog
+        </NavLink>
+
         {false ? <button>Add Blog</button> : '' }
       </div>
     );

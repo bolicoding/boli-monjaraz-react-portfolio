64bf32b82ee83c60697e2d3a145c61f7f76792e7
bolicoding <bolicoding@gmail.com>
2020-10-21 15:52:24 -0600

    imported backgrounds and logos

--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -2,12 +2,17 @@ import React from "react";
 import { Link } from 'react-router-dom';
 
 export default function(props) {
+    // - background image: thumb_image_url
+    // - logo:
+    // - description: description
+    // - id: id
+  const { id, description, thumb_image_url, logo} = props.item;
   return (
     <div>
-      <h3>{props.title}</h3>
-      <h4>{props.url}</h4>
-
-      <Link to={`/portolio/${props.slug}`}>Link</Link>
+      <img src={thumb_image_url} />
+      <img src={logo} />
+      <div>{description}</div>
+      <Link to={`/portolio/${id}`}>Link</Link>
     </div>
   )
 }

28b28c869b0d42636bfea1e50dc9a9aa4dcc80af
bolicoding <bolicoding@gmail.com>
2020-10-21 14:09:25 -0600

    portfolio api query => container class

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from 'axios';
 
 import PortfolioItem from "./portfolio-item";
 
@@ -18,6 +19,18 @@ export default class PortfolioContainer extends Component {
     };
 
     this.handleFilter = this.handleFilter.bind(this);
+    this.getPortfolioItems = this.getPortfolioItems.bind(this);
+  }
+
+  getPortfolioItems() {
+    axios
+      .get('https://bolivarmonjaraz.devcamp.space/portfolio/portfolio_items')
+      .then(response => {
+        console.log("response data", response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 
   handleFilter(filter) {
@@ -38,6 +51,8 @@ export default class PortfolioContainer extends Component {
     if (this.state.isLoading) {
       return <div>Loading...</div>;
     }
+    
+    this.getPortfolioItems();
 
     return (
       <div>

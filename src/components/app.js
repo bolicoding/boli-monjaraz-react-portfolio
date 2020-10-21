c7cdb75c77b699449838666a5fff0b4394a66dd7
bolicoding <bolicoding@gmail.com>
2020-10-21 13:47:50 -0600

    added api from dev space

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import moment from "moment";
+import axios from 'axios';
 import {
   BrowserRouter as Router,
   Switch,
@@ -16,7 +17,24 @@ import NoMatch from './pages/no-match';
 
 
 export default class App extends Component {
+  constructor() {
+    super();
+
+    this.getPortfolioItems = this.getPortfolioItems.bind(this);
+  }
+
+  getPortfolioItems() {
+    axios
+      .get('https://bolivarmonjaraz.devcamp.space/portfolio/portfolio_items')
+      .then(response => {
+        console.log("response data", response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
   render() {
+    this.getPortfolioItems();
     return (
       <div className='app'>
 

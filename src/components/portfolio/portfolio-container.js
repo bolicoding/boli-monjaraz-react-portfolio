edabcd805f734e04b6cf0b5c85c0ca977bf352be
bolicoding <bolicoding@gmail.com>
2020-10-22 15:08:27 -0600

    using mixins to create btn styles

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -52,20 +52,19 @@ export default class PortfolioContainer extends Component {
       return <div>Loading...</div>;
     }
 
-    return (
-      <div>
-        <h2>{this.state.pageTitle}</h2>
-
-        <button onClick={() => this.handleFilter("eCommerce")}>
-          eCommerce
+    return ( 
+      <div className="portfolio-items-wrapper">
+        <button className="btn" onClick={() => this.handleFilter("eCommerce")}>
+        eCommerce
         </button>
-        <button onClick={() => this.handleFilter("Scheduling")}>
+        <button className="btn" onClick={() => this.handleFilter("Scheduling")}>
           Scheduling
         </button>
-        <button onClick={() => this.handleFilter("Enterprise")}>
+        <button className="btn" onClick={() => this.handleFilter("Enterprise")}>
           Enterprise
-        </button>
-    <div className="portfolio-items-wrapper">{this.portfolioItems()}</div>
+        </button>  
+
+        {this.portfolioItems()}
       </div>
     );
   }

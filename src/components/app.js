f6dd907f7431e7dba20310bb76b5b3380334e32b
bolicoding <bolicoding@gmail.com>
2020-10-22 13:28:29 -0600

    cleaned upp app.js

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import moment from "moment";
 import {
   BrowserRouter as Router,
   Switch,
@@ -22,8 +21,6 @@ export default class App extends Component {
 
         <Router>
           <div>
-            <h1>Boli Monjaraz Portfolio</h1>
-            <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
             <NavigationContainer />
 
             <Switch>

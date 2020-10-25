a22e2435c38f909e9779b68b12dbfd87bf5323b3
bolicoding <bolicoding@gmail.com>
2020-10-25 10:43:22 -0600

    sign in error in react

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -7,7 +7,8 @@ export default class Login extends Component {
 
     this.state = {
       email: "",
-      password: ""
+      password: "",
+      errorText: ""
     };
 
     this.handleSubmit = this.handleSubmit.bind(this)
@@ -16,7 +17,8 @@ export default class Login extends Component {
 
   handleChange(event) {
     this.setState({
-      [event.target.name]: event.target.value
+      [event.target.name]: event.target.value,
+      errorText: ""
     });
   }
 
@@ -32,9 +34,20 @@ export default class Login extends Component {
     },
     { withCredentials: true }
   )
-  .then(response => {
-    console.log("response", response);
-  })
+    .then(response => {
+      if (response.data.status === 'created') {
+        console.log('can enter')
+      } else {
+        this.setState({
+          errorText: "wrong email or password"
+        })
+      }
+    })
+    .catch(error => {
+      this.setState({
+        errorText: "An error occurred"
+      })
+    });
 
     event.preventDefault();
   }
@@ -43,6 +56,8 @@ export default class Login extends Component {
     return(
       <div>
         <h1>Login to access your dashboard</h1>
+
+        <div>{this.state.errorText}</div>
 
         <form onSubmit={this.handleSubmit}>
           <input

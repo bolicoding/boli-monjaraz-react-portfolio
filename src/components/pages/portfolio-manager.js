d6b57bf17f60a895c08c39bf557759597bc1fe92
bolicoding <bolicoding@gmail.com>
2020-10-26 11:31:59 -0600

    building portfolio sidebar list comp

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -1,23 +1,30 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
+
+import PortfolioSidebarList from "../portfolio/portfolio-sidebar-list";
 
 export default class PortfolioManager extends Component {
   constructor() {
     super();
 
     this.state = {
-      portfolioItems: []
-    }
+      portfolioItems: [],
+    };
   }
 
   getPortfolioItems() {
-    axios.get("https://bolivarmonjaraz.devcamp.space/portfolio/portfolio_items", { withCredentials: true }).then(response => {
-      this.setState({
-       portfolioItems: [...response.data.portfolioItems] 
+    axios
+      .get("https://bolivarmonjaraz.devcamp.space/portfolio/portfolio_items", {
+        withCredentials: true,
       })
-    }).catch(error => {
-      console.log("error in getPortfolioItems", error)
-    })
+      .then((response) => {
+        this.setState({
+          portfolioItems: [...response.data.portfolio_items],
+        });
+      })
+      .catch((error) => {
+        console.log("error in getPortfolioItems", error);
+      });
   }
 
   componentDidMount() {
@@ -25,16 +32,16 @@ export default class PortfolioManager extends Component {
   }
 
   render() {
-    return(
+    return (
       <div className="portfolio-manager-wrapper">
         <div className="left-column">
           <h1>Portfolio form...</h1>
         </div>
 
         <div className="right-column">
-          <h1>Portfolio sidebar...</h1>
+          <PortfolioSidebarList data={this.state.portfolioItems} />
         </div>
       </div>
-    )
+    );
   }
 }

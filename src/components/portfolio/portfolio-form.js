fac7d1f35ffa59ddc07773b7bbf9f0d8e939a920
bolicoding <bolicoding@gmail.com>
2020-10-26 14:25:37 -0600

    intro to building js formdata objs

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -19,8 +19,20 @@ export class PortfolioForm extends Component {
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  buildForm() {
+    let formData = new FormData();
+
+    formData.append("portfolio_item[name]", this.state.name);
+    formData.append("portfolio_item[description]", this.state.description);
+    formData.append("portfolio_item[url]", this.state.url);
+    formData.append("portfolio_item[category]", this.state.category);
+    formData.append("portfolio_item[position]", this.state.position);
+
+    return formData;
+  }
+
   handleSubmit(event) {
-    console.log("event", event);
+    this.buildForm();
     event.preventDefault();
   }
 

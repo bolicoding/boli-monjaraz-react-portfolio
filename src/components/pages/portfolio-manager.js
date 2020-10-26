0c2a299caefeeab0d969857addd5157438230860
bolicoding <bolicoding@gmail.com>
2020-10-26 11:09:11 -0600

    populating portfolio manager state w api data

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -1,6 +1,29 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class PortfolioManager extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      portfolioItems: []
+    }
+  }
+
+  getPortfolioItems() {
+    axios.get("https://bolivarmonjaraz.devcamp.space/portfolio/portfolio_items", { withCredentials: true }).then(response => {
+      this.setState({
+       portfolioItems: [...response.data.portfolioItems] 
+      })
+    }).catch(error => {
+      console.log("error in getPortfolioItems", error)
+    })
+  }
+
+  componentDidMount() {
+    this.getPortfolioItems();
+  }
+
   render() {
     return(
       <div className="portfolio-manager-wrapper">

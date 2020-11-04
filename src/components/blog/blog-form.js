d1355f6f62cb9a9e19b58ba3e2737d1838a99db4
bolicoding <bolicoding@gmail.com>
2020-11-04 13:24:41 -0700

    fixed error

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -37,19 +37,20 @@ export default class BlogForm extends Component {
   handleSubmit(event) {
     axios
       .post(
-        "https://bolivarmonjaraz.devcamp.space/portfolio/portfolio_blogs",
+        "https://bolivarmonnjaraz.devcamp.space/portfolio/portfolio_blogs",
         this.buildForm(),
         { withCredentials: true }
       )
       .then((response) => {
-        this.props.handleSuccessfullFormSubmission(
-          response.data.portfolio_blog
-        );
-
         this.setState({
           title: "",
           blog_status: "",
+          content: "",
         });
+
+        this.props.handleSuccessfullFormSubmission(
+          response.data.portfolio_blog
+        );
       })
       .catch((error) => {
         console.log("handleSubmit for blog error", error);

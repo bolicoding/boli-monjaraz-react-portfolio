461464837e7f7e55e5b7753977c8178b3d376838
bolicoding <bolicoding@gmail.com>
2020-11-05 13:41:35 -0700

    contact page styles

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -1,5 +1,45 @@
-import React, { Component } from "react";
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import contactPagePicture from "../../../static/assets/images/auth/login.jpg";
 
 export default function () {
-  return <div>Contact</div>;
+  return (
+    <div className="content-page-wrapper">
+      <div
+        className="left-column"
+        style={{
+          background: "url(" + contactPagePicture + ") no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      />
+      <div className="right-column">
+        <div className="contact-bullet-points">
+          <div className="bullet-point-group">
+            <div className="icon">
+              <FontAwesomeIcon icon="phone" />
+            </div>
+
+            <div className="text">385-439-6186</div>
+          </div>
+
+          <div className="bullet-point-group">
+            <div className="icon">
+              <FontAwesomeIcon icon="envelope" />
+            </div>
+
+            <div className="text">bolicoding@gmail.com</div>
+          </div>
+
+          <div className="bullet-point-group">
+            <div className="icon">
+              <FontAwesomeIcon icon="map-marked-alt" />
+            </div>
+
+            <div className="text">West Valley City, UT</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
